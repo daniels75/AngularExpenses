377416898f25777eb127bfab00f04aee3a5a3d05
daniel.sadowski <daniel.sadowski75@gmail.com>
2013-12-27 10:37:57 +0100

    Added simple layout for page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,7 +12,40 @@ angular.module('angularExpensesApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/myroute', {
+        templateUrl: 'views/myroute.html',
+        controller: 'MyrouteCtrl'
+      })
+      .when('/user', {
+        templateUrl: 'views/user.html',
+      })
       .otherwise({
         redirectTo: '/'
       });
   });
+
+function MyRouteController($route, $routeParams, $location) {
+  this.$route = $route;
+  this.$location = $location;
+  this.$routeParams = $routeParams;
+}
+
+
+angular.module('angularExpensesApp').controller('HeaderCtrl', ['$scope', '$location', '$route', '$routeParams',
+  function ($scope, $location, $route, $routeParams) {
+  $scope.location = $location;
+  $scope.pageName = "home";
+
+  $scope.updateCurrentPage = function(currentPageName, $route, $routeParams, $location){
+      this.$route = $route;
+  this.$location = $location;
+  this.$routeParams = $routeParams;
+    $scope.pageName = currentPageName;
+  };
+
+  $scope.isNavbarActive = function (navBarPath) {
+    return navBarPath === $scope.pageName;
+  };
+
+
+}]);
---
+++ app/scripts/controllers/myroute.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularExpensesApp')
+  .controller('MyrouteCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
