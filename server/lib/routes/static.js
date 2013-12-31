3d19364c119fc0e921abedcbe67b37da23b17ccf
daniel.sadowski <daniel.sadowski75@gmail.com>
2013-12-31 11:55:32 +0100

    Added simple server for AE application

---
+++ server/lib/routes/static.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+exports.addRoutes = function(app, config) {
+  // Serve up the favicon
+  console.log('>>>> Dist folder: ' + config.server.distFolder);
+  console.log('>>>> config.server.staticUrl: ' + config.server.staticUrl);
+  
+  app.use(express.favicon(config.server.distFolder + '/favicon.ico'));
+
+  // First looks for a static file: index.html, css, images, etc.
+  app.use(config.server.staticUrl, express.compress());
+  app.use(config.server.staticUrl, express.static(config.server.distFolder));
+  app.use(config.server.staticUrl, function(req, res, next) {
+    res.send(404); // If we get here then the request for a static file is invalid
+  });
+};
